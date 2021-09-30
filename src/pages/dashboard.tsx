6f13cb594d3c843496199b0bfc4d500be918a25a
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-30 11:27:51 -0300

    Siderbar
    Criando a Siderbar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,14 @@
 import { Header } from "../components/Header";
+import { Flex } from "@chakra-ui/react";
+import { Siderbar } from "../components/Siderbar";
 
-export default function Dashboard (){
-  return(
-    <Header />
-  )
+export default function Dashboard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Siderbar />
+      </Flex>
+    </Flex>
+  );
 }
