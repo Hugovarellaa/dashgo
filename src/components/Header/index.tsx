08a30792752037d89077a846ef9c7af352697897
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-01 13:34:38 -0300

    Header
    Header responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "../Header/Logo";
 import { NotifcationsNav } from "../Header/NoticationsNav";
 import { Profile } from "../Header/Profile";
 import { SearchBox } from "../Header/SearchBox";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       as="header"
@@ -17,11 +22,11 @@ export function Header() {
       align="center"
     >
       <Logo />
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotifcationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
