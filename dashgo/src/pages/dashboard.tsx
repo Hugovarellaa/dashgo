ebb229e071902d17726fa3761bd8fb84856c3b7c
Hugo <hugovarellaa@gmail.com>
2022-02-06 16:06:56 -0300

    Criando a siderbar

--- dashgo/src/pages/dashboard.tsx
+++ dashgo/src/pages/dashboard.tsx
@@ -1,9 +1,14 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header/index";
+import { Siderbar } from "../components/Siderbar";
 
 export default function dashboard() {
   return (
-    <>
+    <Flex direction="column" height="100vh">
       <Header />
-    </>
+      <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Siderbar />
+      </Flex>
+    </Flex>
   );
 }
