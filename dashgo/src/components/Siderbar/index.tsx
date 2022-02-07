c089c44548904fd7f715e90ebd20d4e3f3188e80
Hugo <hugovarellaa@gmail.com>
2022-02-07 08:24:25 -0300

    Criando Siderbar responsiva

--- dashgo/src/components/Siderbar/index.tsx
+++ dashgo/src/components/Siderbar/index.tsx
@@ -1,7 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { SiderBarNav } from "./SiderBarNav";
 
 export function Siderbar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (isDrawerSidebar) {
+    return (
+      <Drawer isOpen={true} placement="left" onClose={() => {}}>
+        <DrawerOverlay>
+          <DrawerContent backgroundColor="gray.800" padding="4">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerBody>
+              <SiderBarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
+  }
+
   return (
     <Box as="aside" width="64" mr="8">
       <SiderBarNav />
