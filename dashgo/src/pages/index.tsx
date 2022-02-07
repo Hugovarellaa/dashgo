7d230dab4c27025ab73bd4020c012b01851c411a
hugovarellaa <hugovarellaa@gmail.com>
2022-02-07 15:43:53 -0300

    Adicionando react-hook-form e configurando ele no Home-page

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,7 +1,20 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (value) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(value);
+  };
+
   return (
     <Flex width="100vw" height="100vh" align="center" justify="center">
       <Flex
@@ -12,13 +25,30 @@ export default function SignIn() {
         padding="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Password" type="password" />
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            label="Password"
+            type="password"
+            {...register("password")}
+          />
         </Stack>
 
-        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
