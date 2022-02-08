d4e845e6d441ca1fa54dae6a911b613fba0e9014
hugovarellaa <hugovarellaa@gmail.com>
2022-02-08 08:59:40 -0300

    Spinner de reload no heading de Usuario

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -24,7 +24,7 @@ import { Siderbar } from "../../components/Siderbar";
 import { useQuery } from "react-query";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, error, isFetching } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -70,6 +70,9 @@ export default function UserList() {
           <Flex marginBottom="8" justifyContent="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
