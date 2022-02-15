f7bce5498f289d5318821698ea3185f574e05811
hugovarellaa <hugovarellaa@gmail.com>
2022-02-15 13:18:13 -0300

    componentizado Input na page Sign in

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,12 +1,6 @@
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input as ChakraInput,
-  Stack,
-} from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+
 export default function Home() {
   return (
     <>
