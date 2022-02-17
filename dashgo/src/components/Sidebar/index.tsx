acaf4c640a3a8446a98151970e1543dc39345862
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 10:09:46 -0300

    Session dashboard

--- dashgo/src/components/Sidebar/index.tsx
+++ dashgo/src/components/Sidebar/index.tsx
@@ -1,3 +1,24 @@
+import { Box, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import { RiDashboardLine } from "react-icons/ri";
+
 export function Sidebar() {
-  return <h1>Testando</h1>;
+  return (
+    <Box as="aside" width="64" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            GERAL
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiDashboardLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
 }
