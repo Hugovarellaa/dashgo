c0d7cba80618540f84edff839261f532f15356df
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 16:29:27 -0300

    Criando sidebar

--- dashgo/src/components/Sidebar/index.tsx
+++ dashgo/src/components/Sidebar/index.tsx
@@ -1,7 +1,38 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (isDrawerSidebar) {
+    return (
+      <Drawer isOpen={true} onClose={() => {}} placement="left">
+        <DrawerOverlay>
+          <DrawerContent background="gray.800" p="4">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navegação</DrawerHeader>
+
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
+  }
+
   return (
     <Box as="aside" width="64" mr="8">
       <SidebarNav />
