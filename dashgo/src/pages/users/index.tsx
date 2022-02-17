0dfb98d268b69b3d59046635891a780c705ed37c
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 10:55:09 -0300

    heading and button ADD User

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
@@ -11,7 +12,23 @@ export default function UserList() {
         <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="10">
           <Sidebar />
 
-          <Box flex="1" borderRadius={8} background="gray.800" p="8"></Box>
+          <Box flex="1" borderRadius={8} background="gray.800" p="8">
+            <Flex mb="8" justify="space-between" align="center">
+              <Heading fontSize="lg" fontWeight="normal">
+                Usuários
+              </Heading>
+
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Flex>
+          </Box>
         </Flex>
       </Box>
     </>
