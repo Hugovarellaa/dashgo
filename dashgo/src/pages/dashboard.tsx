57a2f294900cb85b6b8a97b94bdbc90a10c03e64
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 10:44:42 -0300

    Config graphic

--- dashgo/src/pages/dashboard.tsx
+++ dashgo/src/pages/dashboard.tsx
@@ -1,11 +1,58 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-const options = {};
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[500],
+    },
+    axisTicks: {
+      color: theme.colors.gray[500],
+    },
+    categories: [
+      "2021-03-10T00:00:00.000Z",
+      "2021-03-11T00:00:00.000Z",
+      "2021-03-12T00:00:00.000Z",
+      "2021-03-13T00:00:00.000Z",
+      "2021-03-14T00:00:00.000Z",
+      "2021-03-15T00:00:00.000Z",
+      "2021-03-16T00:00:00.000Z",
+      "2021-03-17T00:00:00.000Z",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+} as const;
 
 const series = [{ name: "series1", data: [10, 100, 23, 150, 11, 109, 37, 72] }];
 
@@ -34,6 +81,7 @@ export default function Dashboard() {
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
+            <Chart type="area" height={160} options={options} series={series} />
           </Box>
         </SimpleGrid>
       </Flex>
