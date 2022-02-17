c5bfb47d144214529e67ab88d7d73b07bf330d89
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 10:22:23 -0300

    Create area for graphic

--- dashgo/src/pages/dashboard.tsx
+++ dashgo/src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
@@ -9,6 +9,25 @@ export default function Dashboard() {
 
       <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="10">
         <Sidebar />
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="300px"
+          alignItems="flex-start"
+        >
+          <Box p="8" background="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+          </Box>
+
+          <Box p="8" background="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
