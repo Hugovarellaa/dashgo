d6db43fd24a3c54e013674261d493e007c0d8ba5
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 10:50:03 -0300

    Insert Header and sidebar in users

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,3 +1,18 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
 export default function UserList() {
-  return <h1>testando</h1>;
+  return (
+    <>
+      <Box>
+        <Header />
+
+        <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="10">
+          <Sidebar />
+          asdfasd
+        </Flex>
+      </Box>
+    </>
+  );
 }
