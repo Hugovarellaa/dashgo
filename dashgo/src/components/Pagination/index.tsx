33a33f8a98ed8ccc25a90a9148695deab3dbd90e
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 11:30:54 -0300

    layout base pagination

--- dashgo/src/components/Pagination/index.tsx
+++ dashgo/src/components/Pagination/index.tsx
@@ -1,3 +1,53 @@
-export function Pagination (){
-    return()
+import { Box, Button, HStack, Stack } from "@chakra-ui/react";
+
+export function Pagination() {
+  return (
+    <Stack
+      flexDirection="row"
+      spacing="6"
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong> 10 </strong> de <strong>100</strong>
+      </Box>
+      <HStack spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{ background: "pink.500", cursor: "default" }}
+        >
+          1
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          background="gray.700"
+          _hover={{
+            background: "pink.500",
+          }}
+        >
+          2
+        </Button>
+
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          background="gray.700"
+          _hover={{
+            background: "pink.500",
+          }}
+        >
+          3
+        </Button>
+      </HStack>
+    </Stack>
+  );
 }
