a5ddcda5977d8bc758de1e46fc08e385bd6076f3
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 09:20:34 -0300

    Logo da aplicação

--- dashgo/src/components/Header/index.tsx
+++ dashgo/src/components/Header/index.tsx
@@ -1,7 +1,26 @@
+import { Flex, Text } from "@chakra-ui/react";
+
 export function Header() {
   return (
     <>
-      <h1>testando</h1>
+      <Flex
+        as="header"
+        width="100%"
+        maxWidth={1480}
+        height="20"
+        mx="auto"
+        mt="4"
+        px="10"
+        align="center"
+      >
+        {/* Logo */}
+        <Text fontSize="3xl" letterSpacing="tight" width="64">
+          DashGo
+          <Text as="span" color="pink.500" m="1">
+            .
+          </Text>
+        </Text>
+      </Flex>
     </>
   );
 }
