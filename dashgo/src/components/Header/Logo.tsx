e20555348724e9711e955680699a2a4226dc05dd
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 15:59:27 -0300

    Update Logo.tsx

--- dashgo/src/components/Header/Logo.tsx
+++ dashgo/src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
   return (
-    <Text fontSize="3xl" letterSpacing="tight" width="64">
+    <Text fontSize={["2xl", "3xl"]} letterSpacing="tight" width="64">
       DashGo
       <Text as="span" color="pink.500" m="1">
         .
