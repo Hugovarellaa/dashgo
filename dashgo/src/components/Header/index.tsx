4bccf28330ade859a5bcc69f651d80911e5b2958
hugovarellaa <hugovarellaa@gmail.com>
2022-02-17 09:26:46 -0300

    Layou Search box do header

--- dashgo/src/components/Header/index.tsx
+++ dashgo/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Icon, Input, Text } from "@chakra-ui/react";
+import { RiSearchLine } from "react-icons/ri";
 
 export function Header() {
   return (
@@ -20,6 +21,33 @@ export function Header() {
             .
           </Text>
         </Text>
+
+        {/* SearchBox */}
+        <Flex
+          as="label"
+          flex="1"
+          py="4"
+          px="8"
+          ml="6"
+          maxWidth={400}
+          alignSelf="center"
+          color="gray.200"
+          position="relative"
+          background="gray.800"
+          borderRadius="full"
+        >
+          <Input
+            color="gray.50"
+            variant="unstyled"
+            px="4"
+            mr="4"
+            placeholder="Buscar na plataforma"
+            _placeholder={{
+              color: "gray.400",
+            }}
+          />
+          <Icon as={RiSearchLine} fontSize="20" />
+        </Flex>
       </Flex>
     </>
   );
