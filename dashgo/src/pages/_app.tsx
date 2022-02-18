e04e4c0578f5278bc83513545a8e3f63742235e1
hugovarellaa <hugovarellaa@gmail.com>
2022-02-18 16:05:45 -0300

    Configurando React-Query

--- dashgo/src/pages/_app.tsx
+++ dashgo/src/pages/_app.tsx
@@ -3,17 +3,22 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SidebarProvide } from "../context/SidebarDrawerContext";
 import { makeServer } from "../services/mirage-js";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <SidebarProvide>
-        <Component {...pageProps} />
-      </SidebarProvide>
+      <QueryClientProvider client={queryClient}>
+        <SidebarProvide>
+          <Component {...pageProps} />
+        </SidebarProvide>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
