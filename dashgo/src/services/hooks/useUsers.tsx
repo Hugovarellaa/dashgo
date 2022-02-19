1745e1d86b839d63d25983134c52e807fdc484b2
hugovarellaa <hugovarellaa@gmail.com>
2022-02-18 21:30:43 -0300

    refatorando o hook userUsers

--- dashgo/src/services/hooks/useUsers.tsx
+++ dashgo/src/services/hooks/useUsers.tsx
@@ -1,27 +1,37 @@
 import { useQuery } from "react-query";
 import { api } from "../axios/api";
 
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createAt: string;
+};
+
+export async function getUsers(): Promise<User[]> {
+  const { data } = await api.get("http://localhost:3000/api/users");
+  //formatação dos dados
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createAt: new Date(user.createAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
+  return users;
+}
+
 export function useUsers() {
   return useQuery(
     "users",
-    async () => {
-      const { data } = await api.get("http://localhost:3000/api/users");
-      //formatação dos dados
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createAt: new Date(user.createAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
+    getUsers,
 
-      return users;
-    },
     {
       staleTime: 1000 * 5,
     }
