cea5b226c8a621717cfbbb9d9f089dbdbe7144f8
hugovarellaa <hugovarellaa@gmail.com>
2022-02-19 15:14:14 -0300

    Redirecionando para dashboard caso ja tenha feito login

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useAuth } from "../context/AuthContext";
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
 
 interface IFormValues {
   email: string;
@@ -72,3 +74,18 @@ export default function SignIn() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies["nextauth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
