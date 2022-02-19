47e5bef8a2e7412b0d654d0db963ec4177d97bae
hugovarellaa <hugovarellaa@gmail.com>
2022-02-18 22:48:53 -0300

    refatorando queryClient no app.tsx

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Link,
   Spinner,
   Table,
   Tbody,
@@ -15,7 +16,7 @@ import {
   Tr,
   useBreakpointValue,
 } from "@chakra-ui/react";
-import Link from "next/link";
+import NextLink from "next/link";
 import { useState } from "react";
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -34,6 +35,8 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  function handlePrefetchUser(id: number) {}
 
   return (
     <>
@@ -56,7 +59,7 @@ export default function UserList() {
                   <Spinner size="sm" color="gray.500" ml="4" />
                 )}
               </Heading>
-              <Link href="/users/create" passHref>
+              <NextLink href="/users/create" passHref>
                 <Button
                   as="a"
                   size="sm"
@@ -67,7 +70,7 @@ export default function UserList() {
                 >
                   Criar novo
                 </Button>
-              </Link>
+              </NextLink>
             </Flex>
 
             {isLoading ? (
@@ -99,7 +102,12 @@ export default function UserList() {
                           </Td>
                           <Td>
                             <Box>
-                              <Text fontWeight="bold">{user.name}</Text>
+                              <Link
+                                color="purple.400"
+                                onMouseEnter={() => handlePrefetchUser(user.id)}
+                              >
+                                <Text fontWeight="bold">{user.name}</Text>
+                              </Link>
                               <Text fontSize="sm" color="gray.300">
                                 {user.email}
                               </Text>
