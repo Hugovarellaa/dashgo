61afc55ee3eea6d4984ab0988a87d354fe3730ca
hugovarellaa <hugovarellaa@gmail.com>
2022-02-18 22:16:15 -0300

    logica de paginação das entre paginas

--- dashgo/src/components/Pagination/index.tsx
+++ dashgo/src/components/Pagination/index.tsx
@@ -1,7 +1,47 @@
 import { Box, HStack, Stack } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePageArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  //Ultima pagina
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  //Pagina anterior
+  const previousPages =
+    currentPage > 1
+      ? generatePageArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  //Proxima Pagina
+  const nextPages =
+    currentPage < lastPage
+      ? generatePageArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : [];
+
   return (
     <Stack
       flexDirection={["column", "column", "row"]}
@@ -14,15 +54,20 @@ export function Pagination() {
         <strong>0</strong> - <strong> 10 </strong> de <strong>100</strong>
       </Box>
       <HStack spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
-        <PaginationItem number={5} />
-        <PaginationItem number={6} />
-        <PaginationItem number={7} />
-        <PaginationItem number={8} />
-        <PaginationItem number={9} />
+        {/* anteriores */}
+        {previousPages.length > 0 &&
+          previousPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        {/* atual */}
+        <PaginationItem number={currentPage} isCurrent />
+
+        {/* Proxima */}
+        {nextPages.length > 0 &&
+          nextPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
       </HStack>
     </Stack>
   );
