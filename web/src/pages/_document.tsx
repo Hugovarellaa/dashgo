edee1da3e4fa275278b842e04e437be211167063
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-31 17:42:33 -0300

    Config chakra-ui

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript, theme } from '@chakra-ui/react'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
         <Main />
         <NextScript />
       </body>
