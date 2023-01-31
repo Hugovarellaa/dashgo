0fbd15beec13db5a86286a03913fe3fb76e95606
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-31 19:46:43 -0300

    Profile

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -46,6 +46,36 @@ export function Header() {
           marginRight="4"
         />
         <Icon as={RiSearchLine} fontSize={20} />
+      </Flex>
+
+      {/* Profile */}
+      <Flex alignItems="center" marginLeft="auto">
+        <HStack
+          spacing="8"
+          marginX="8"
+          paddingRight="8"
+          paddingY="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize={20} />
+          <Icon as={RiUserAddLine} fontSize={20} />
+        </HStack>
+
+        <Flex alignItems="center">
+          <Box marginRight="4" textAlign="right">
+            <Text>Hugo Alves Varella</Text>
+            <Text color="gray.300" fontSize="small">
+              hugovarellaa@gmail.com
+            </Text>
+          </Box>
+          <Avatar
+            size="md"
+            name="Hugo Varella"
+            src="https://github.com/Hugovarellaa.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
