478e3f5472dd005fc7dc1c96b74acbfe032bb1e2
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-01 17:20:05 -0300

    Configurando Miragejs API-Faker

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext'
+import { makeServer } from '../services/miragejs'
 import { theme } from '../styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
---
+++ web/src/services/miragejs.ts
@@ -0,0 +1,31 @@
+import { createServer, Model } from 'miragejs'
+
+type User = {
+  name: string
+  email: string
+  created_at: string
+}
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+
+    routes() {
+      this.namespace = 'api'
+      this.timing = 750
+
+      // Shorthand
+      this.get('/users')
+      this.post('/users')
+
+      // Retorno para vazia as rotas do mirage.js, assim nao prejudicar as API Routes do next
+      this.namespace = ''
+
+      this.passthrough()
+    },
+  })
+
+  return server
+}
