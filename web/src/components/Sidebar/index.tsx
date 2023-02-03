4f894515e6588ed1d3832f92599054d01620f6f8
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-03 10:57:49 -0300

    Sidebar components

--- web/src/components/Sidebar/index.tsx
+++ web/src/components/Sidebar/index.tsx
@@ -1,3 +1,57 @@
+import { Box, Icon, Link, Stack, Text } from '@chakra-ui/react'
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from 'react-icons/ri'
+
 export function Sidebar() {
-  return <h1>sidebar</h1>
+  return (
+    <Box as="aside" width="64" marginRight={8}>
+      <Stack spacing="12" alignItems="flex-start">
+        <Box>
+          <Text fontWeight="bold" fontSize="small" color="gray.400">
+            GERAL
+          </Text>
+          <Stack spacing="4" marginTop="8" alignItems="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiDashboardLine} fontSize="20" />
+              <Text marginLeft="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+
+            <Link display="flex" alignItems="center">
+              <Icon as={RiContactsLine} fontSize="20" />
+              <Text marginLeft="4" fontWeight="medium">
+                Usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontWeight="bold" fontSize="small" color="gray.400">
+            AUTOMAÇÃO
+          </Text>
+          <Stack spacing="4" marginTop="8" alignItems="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiInputMethodLine} fontSize="20" />
+              <Text marginLeft="4" fontWeight="medium">
+                Formulário
+              </Text>
+            </Link>
+
+            <Link display="flex" alignItems="center">
+              <Icon as={RiGitMergeLine} fontSize="20" />
+              <Text marginLeft="4" fontWeight="medium">
+                Automação
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  )
 }
