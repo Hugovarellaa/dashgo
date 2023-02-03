747ad87dac10d164097f2131f1a437fbad3e99eb
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-03 09:56:41 -0300

    Config chakra-ui SSR

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,4 +1,6 @@
+import { ColorModeScript } from '@chakra-ui/react'
 import { Head, Html, Main, NextScript } from 'next/document'
+import { theme } from '../styles/theme'
 
 export default function Document() {
   return (
@@ -12,6 +14,8 @@ export default function Document() {
         />
       </Head>
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
         <Main />
         <NextScript />
       </body>
