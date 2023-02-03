2df650fdabbcc792049e67d76c40681d87d1d890
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-03 10:31:54 -0300

    Create Logo in Header

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,7 +1,24 @@
+import { Flex, Text } from '@chakra-ui/react'
+
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Flex
+      as="header"
+      width="100%"
+      maxWidth={1480}
+      height="20"
+      marginX="auto"
+      marginTop="4"
+      paddingX="6"
+      alignItems="center"
+    >
+      {/* Logo */}
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" width="64">
+        DashGo
+        <Text as="span" color="pink.500" marginLeft="1">
+          .
+        </Text>
+      </Text>
+    </Flex>
   )
 }
