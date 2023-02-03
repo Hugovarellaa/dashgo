4a101990bfcb5d518f4e6b3087a4246000c58361
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-03 10:38:49 -0300

    Create SearchBox in header

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Icon, Input, Text } from '@chakra-ui/react'
+import { RiSearchLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -19,6 +20,33 @@ export function Header() {
           .
         </Text>
       </Text>
+
+      {/* SearchBox */}
+      <Flex
+        as="label"
+        flex={1}
+        paddingY="4"
+        paddingX="8"
+        marginLeft="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        backgroundColor="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          placeholder="Buscar na plataforma"
+          _placeholder={{
+            color: 'gray.400',
+          }}
+          paddingX="4"
+          marginRight="4"
+        />
+        <Icon as={RiSearchLine} fontSize={18} />
+      </Flex>
     </Flex>
   )
 }
