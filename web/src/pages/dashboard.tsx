346cba99f5a94d5e9613044922d9b5c3a0ed8cdb
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-04 14:17:24 -0300

    Create page Dashboard

---
+++ web/src/pages/dashboard.tsx
@@ -0,0 +1,35 @@
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { useAuth } from "../context/AuthContext";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+export default function Dashboard() {
+  const { user } = useAuth();
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignItems="flex-start"
+        >
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Incritos da semana
+            </Text>
+          </Box>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
+}
