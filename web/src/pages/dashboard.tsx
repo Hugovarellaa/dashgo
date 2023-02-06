ace23bcee580c86a6f29a9a05bee828f4f0045f3
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-06 12:00:53 -0300

    Layout sidebar start

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,5 +1,7 @@
+import { Flex } from '@chakra-ui/react'
 import Head from 'next/head'
 import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
 
 export default function Dashboard() {
   return (
@@ -7,9 +9,19 @@ export default function Dashboard() {
       <Head>
         <title>Dashboard</title>
       </Head>
-      <div>
+      <Flex direction="column" height="100vh">
         <Header />
-      </div>
+
+        <Flex
+          width="100%"
+          marginY="6"
+          maxWidth={1480}
+          marginX="auto"
+          paddingX="6"
+        >
+          <Sidebar />
+        </Flex>
+      </Flex>
     </>
   )
 }
