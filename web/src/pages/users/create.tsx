8aa0388dd68b9edf1ae35187c5eb9e536fc364f1
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-06 20:03:03 -0300

    Validação de formulario Create users

--- web/src/pages/users/create.tsx
+++ web/src/pages/users/create.tsx
@@ -11,9 +11,37 @@ import {
   SimpleGrid,
   VStack,
 } from '@chakra-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
+
+const schema = zod
+  .object({
+    name: zod.string().min(4, 'O nome precisa ter no mínimo 4 caracteres'),
+    email: zod.string().email('Digite um email valido'),
+    password: zod.string().min(6, 'A senha precisa ter no mínimo 6 caracteres'),
+    password_confirmation: zod.string(),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: 'As senhas precisam ser iguais',
+    path: ['password_confirmation'], // path of error
+  })
+
+type formData = zod.infer<typeof schema>
 
 export default function CreateUser() {
+  const { register, handleSubmit, formState } = useForm<formData>({
+    resolver: zodResolver(schema),
+  })
+
+  const { errors, isSubmitting } = formState
+
+  async function handleCreateUsers(data: formData) {
+    return new Promise((resolve) => setTimeout(resolve, 3000))
+    // console.log(data)
+  }
+
   return (
     <Box>
       <Header />
@@ -32,6 +60,8 @@ export default function CreateUser() {
           borderRadius={8}
           backgroundColor="gray.800"
           padding={['6', '8']}
+          as="form"
+          onSubmit={handleSubmit(handleCreateUsers)}
         >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
@@ -40,15 +70,34 @@ export default function CreateUser() {
 
           <VStack spacing={['6', '8']}>
             <SimpleGrid minChildWidth="240px" spacing={['6', '8']} width="100%">
-              <Input name="name" label="Nome Completo" />
-              <Input name="email" label="E-mail" type="email" />
+              <Input
+                name="name"
+                label="Nome Completo"
+                {...register('name')}
+                error={errors.name}
+              />
+              <Input
+                name="email"
+                label="E-mail"
+                type="email"
+                {...register('email')}
+                error={errors.email}
+              />
             </SimpleGrid>
             <SimpleGrid minChildWidth="240px" spacing={['6', '8']} width="100%">
-              <Input name="password" label="Senha" type="password" />
+              <Input
+                name="password"
+                label="Senha"
+                type="password"
+                {...register('password')}
+                error={errors.password}
+              />
               <Input
                 type="password"
                 name="password_confirmation"
                 label="Confirmação de senha"
+                {...register('password_confirmation')}
+                error={errors.password_confirmation}
               />
             </SimpleGrid>
           </VStack>
@@ -56,9 +105,22 @@ export default function CreateUser() {
           <Flex align="center" justifyContent="flex-end" marginTop={['6', '8']}>
             <HStack spacing="4">
               <Link href="/users">
-                <Button colorScheme="whiteAlpha">Cancelar</Button>
+                <Button
+                  colorScheme="whiteAlpha"
+                  isLoading={isSubmitting}
+                  disabled={isSubmitting}
+                >
+                  Cancelar
+                </Button>
               </Link>
-              <Button colorScheme="purple">Salvar</Button>
+              <Button
+                colorScheme="purple"
+                type="submit"
+                isLoading={isSubmitting}
+                disabled={isSubmitting}
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
