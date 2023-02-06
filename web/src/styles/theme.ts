902af7dc5105cd25f478b2160a85ff4733accbf0
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-06 10:30:19 -0300

    Bug to write

--- web/src/styles/theme.ts
+++ web/src/styles/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   styles: {
-    globa: {
+    global: {
       body: {
         bg: "gray.900",
         color: "gray.50",
