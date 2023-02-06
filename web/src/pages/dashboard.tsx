3544d7d264030bdcd37d635670b45d36e93c8964
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-06 12:25:03 -0300

    Update Dashboard

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
 import Head from 'next/head'
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
@@ -20,6 +20,34 @@ export default function Dashboard() {
           paddingX="6"
         >
           <Sidebar />
+
+          <SimpleGrid
+            flex={1}
+            gap="4"
+            minChildWidth="320px"
+            alignItems="flex-start"
+          >
+            <Box padding="8" backgroundColor="gray.800" borderRadius={8}>
+              <Text
+                padding="8"
+                backgroundColor="gray.800"
+                borderRadius={8}
+                paddingBottom="4"
+              >
+                Box 1
+              </Text>
+            </Box>
+            <Box padding="8" backgroundColor="gray.800" borderRadius={8}>
+              <Text
+                padding="8"
+                backgroundColor="gray.800"
+                borderRadius={8}
+                paddingBottom="4"
+              >
+                Box 2
+              </Text>
+            </Box>
+          </SimpleGrid>
         </Flex>
       </Flex>
     </>
