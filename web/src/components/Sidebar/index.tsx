2da67af176284217e0e018010fdfbc5d36bcf9e4
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-06 16:35:32 -0300

    Using Provider context sidebardrawer

--- web/src/components/Sidebar/index.tsx
+++ web/src/components/Sidebar/index.tsx
@@ -1,8 +1,34 @@
-import { Box } from '@chakra-ui/react'
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useBreakpointValue,
+} from '@chakra-ui/react'
 import { SidebarNav } from './SidebarNav'
 
 export function Sidebar() {
-  return (
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  })
+
+  return isDrawerSidebar ? (
+    <Drawer isOpen={true} placement="left" onClose={() => {}}>
+      <DrawerOverlay>
+        <DrawerContent backgroundColor="gray.800" padding="4">
+          <DrawerCloseButton marginTop="6" />
+          <DrawerHeader>Navegação</DrawerHeader>
+          <DrawerBody>
+            <SidebarNav />
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  ) : (
     <Box as="aside" width="64" marginRight="8">
       <SidebarNav />
     </Box>
